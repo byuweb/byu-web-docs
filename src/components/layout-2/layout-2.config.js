e95dc65f7177d1194e7cf517290c67e6beff5cb7
Nathan Walton <nwalton@byu.edu>
2018-12-04 13:48:28 -0700

    Add roboto option

--- src/components/layout-2/layout-2.config.js
+++ src/components/layout-2/layout-2.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "Layout (Open Sans)",
-  label: "Layout (Open Sans)",
+  title: "Layout (Roboto)",
+  label: "Layout (Roboto)",
   collated: true,
   collator: function(markup, item) {
     return `<div class="variant"><h2 class="label">${item.label}</h2> <a class="link" target="_blank" href="../preview/${item.handle}">View</a> <div class="content">${markup}</div> </div>`;
