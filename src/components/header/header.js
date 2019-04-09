7269e7107d28f26fb0d043149ea7f76632deaf25
Nathan Walton <nwalton@byu.edu>
2019-04-09 17:17:40 -0600

    Basic js enabled for header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,25 +4,87 @@ var byu_header = (function() {
 
 	var doc = document; // Put this in a variable to enable smaller compression
 	var jsInitClass = 'js-enabled';
-	var headerEl;
+	var expandedAttr = 'aria-expanded';
+	var menuOpenClass = 'menu-open';
 
 	// Execute the initialize function
 	byu_main.executeOnLoad( init );
 
 
+
+
+
 	/*
-	 * Func: Init
-	 * Desc: Initialize the header button
+	 * Func: init
+	 * Desc: Initialize the header button for all BYU headers on page
+	 * 		 (handles multiple heders )
 	 * Args: none
 	 */
 	function init() {
-		headerEl = doc.getElementsByClassName('byu-header')[0];
-		headerEl.addClass(jsInitClass);
-		if( headerEl.hasClass(jsInitClass) ) {
-			console.log( 'Header js initialized' );
+
+		var headerEls = doc.getElementsByClassName('byu-header');
+
+		Array.prototype.forEach.call(headerEls, function(el) {
+			enableMobileMenu( el );
+		});
+
+	} // init
+
+
+
+
+	/*
+	 * Func: enableMobileMenu
+	 * Desc: Initialize the header button
+	 * Args: headerEl - HTML Element - Probably the <header>
+	 */
+	function enableMobileMenu( headerEl ) {
+
+		var showMenuButton = byu_main.elExists('byu-site-navigation', headerEl) || byu_main.hasChildren('byu-action-id-search', headerEl);
+
+		if ( showMenuButton ) {
+			headerEl.addClass(jsInitClass);
+			initMenuButton( headerEl );
+		}
+
+	} // enableMobileMenu
+
+
+
+	function initMenuButton( headerEl ) {
+		var menuButton = headerEl.getElementsByClassName('byu-menu-button')[0];
+
+		menuButton.addEventListener('click', function(e){
+			clickMenuButton( e.target, headerEl );
+		});
+	}
+
+
+	function clickMenuButton( btn, headerEl ) {
+		var open = btn.getAttribute(expandedAttr) != 'false';
+		var opening = false;
+		
+		if ( !open && !opening ) {
+			openMenu( btn, headerEl );
+		}
+
+		else {
+			closeMenu( btn, headerEl );
 		}
 	}
 
-	return {};
+
+	function openMenu( btn, headerEl ) {
+		btn.setAttribute(expandedAttr, true);
+		if ( !headerEl.hasClass( menuOpenClass ) ) {
+			headerEl.addClass( menuOpenClass )		
+		}
+	}
+
+	function closeMenu( btn, headerEl ) {
+		btn.setAttribute(expandedAttr, false);
+		headerEl.removeClass( menuOpenClass );
+	}
+
 
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -33,33 +33,90 @@ var byu_main = (function(){
 	// RemoveClass
 	HTMLElement.prototype.removeClass = function(name) {
 	  if (this.hasClass(name)) {
-	    var reg = new RegExp('(\\s|^)'+name+'(\\s|$)');
-	    this.className = this.className.replace(reg,' ');
+	    var search = new RegExp('(\\s|^)'+name+'(\\s|$)'); // RegEx to identify match
+	    var replace = new RegExp('(\\s|^)'+name); // RegEx to replace if matched
+	    if ( this.className.search(search) ) {
+	    	this.className = this.className.replace(replace,'');
+	    }
 	  }
 	  return this;
 	};
 
 
+
+
+
 	/*
-	 * Func: ExecuteOnLoad
+	 * Func: executeOnLoad
 	 * Desc: Execute specified function when the DOM is ready. Handle 
-	 * Args: functionName - Function Identifier - Name of the function to execute
+	 * Args: @functionName - Function Identifier - Name of the function to execute
 	 */
 	function executeOnLoad( functionName ) {
+
 		// in case the document is already rendered
 		if (doc.readyState!='loading') functionName();
+
 		// modern browsers
 		else if (doc.addEventListener) doc.addEventListener('DOMContentLoaded', functionName);
+
 		// IE <= 8
 		else doc.attachEvent('onreadystatechange', function(){
 		    if (doc.readyState=='complete') functionName();
 		});
-	}
+
+	} // executeOnLoad
+
+
+
+
+	/*
+	 * Func: elExists
+	 * Desc: Check whether an element appears in the DOM
+	 * Args: @className - String - Name of the class on the element to search for
+	 * 		 @container - HTML Element - Context for finding the element (could be document)
+	 */
+	function elExists( className, container ) {
+
+		return container.getElementsByClassName( className ).length > 0;
+
+	} // elExists
+
+
+
+
+	/*
+	 * Func: hasChildren
+	 * Desc: Check whether an element has children
+	 * Args: @className - String - Name of the element's class
+	 * 		 @container - HTML Element - Context for finding the element (could be document)
+	 */
+	function hasChildren( className, container ) {
+
+		var hasChildren = false;
+		var els;
+
+		if ( elExists( className, container ) ) {
+			els = container.getElementsByClassName( className );
+
+			Array.prototype.forEach.call(els, function(el) {
+				if (el.children.length > 0) {
+					hasChildren = true;
+				} 
+			});
+		}
+
+		return hasChildren;
+
+	} // hasChildren
+
+
 
 
 	// Expose public methods
 	return {
 		executeOnLoad: executeOnLoad,
+		elExists: elExists,
+		hasChildren: hasChildren,
 	}
 
 })();
